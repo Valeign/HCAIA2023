f2fae6349e7260f25104f1fc093f1b09bed0b7e9
Valentina <valentina.contrerasr@userena.cl>
2023-10-06 12:46:09 -0300

    Agregue un supermercado

---
+++ clases/clases6oct/Programa.py
@@ -0,0 +1,8 @@
+# Diseño y programacion de un supermercados
+
+#ver cuantas personas entran
+#cuantas necesitan para atender
+#
+
+#objetivo: calcular el tiempo de espera
+
---
+++ clases/clases6oct/personas.py
@@ -0,0 +1,26 @@
+class Personas():
+	"""Esta es una clase para almacenar personas"""
+	
+	def __init__(self()):
+		self.items = 0
+		self.ilocal = 0 #El tiempo total del super
+
+class validor(Personas):
+	""" Esta es una clase para representar personas que validen """
+
+	def __init__(self, fac=10):
+		" Este es el factor de conversion items a tiempo de validar"
+		self.validar = fac
+		self.validationTime = 0
+
+	def get_item(self, nitems):
+		self.items = nitems
+		self.validationTime = nitem*self.validfactor
+
+	def validar(self,t):
+		while (0<t and 0< self.validantionTime ):
+			self.validationTime -=1
+			dt -= 1
+		return dt 
+
+
